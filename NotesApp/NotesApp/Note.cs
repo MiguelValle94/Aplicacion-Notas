de2e5f3fd7dc7065462e350b87f1e2358c19aa54
miguelbachi5a <miguelbachi5a@gmail.com>
2021-01-11 19:12:02 +0100

    Create class Note

---
+++ NotesApp/NotesApp/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesApp
+{
+    class Note
+    {
+        public string Title { get; set; }
+        public string Notes { get; set; }
+    }
+}
