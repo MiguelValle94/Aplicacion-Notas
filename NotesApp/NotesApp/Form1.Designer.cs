e5b6dd0edc8f56b2824b71d4b4e578a84496fe30
miguelbachi5a <miguelbachi5a@gmail.com>
2021-01-12 18:39:04 +0100

    Add notes and Clear the form

--- NotesApp/NotesApp/Form1.Designer.cs
+++ NotesApp/NotesApp/Form1.Designer.cs
@@ -29,13 +29,133 @@ namespace NotesApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.grdNotes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Notes";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(78, 37);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(275, 23);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(78, 71);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(275, 248);
+            this.txtNotes.TabIndex = 3;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(78, 334);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(278, 334);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(444, 334);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 7;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(568, 334);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // grdNotes
+            // 
+            this.grdNotes.AllowUserToAddRows = false;
+            this.grdNotes.AllowUserToDeleteRows = false;
+            this.grdNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdNotes.Location = new System.Drawing.Point(444, 40);
+            this.grdNotes.Name = "grdNotes";
+            this.grdNotes.ReadOnly = true;
+            this.grdNotes.RowTemplate.Height = 25;
+            this.grdNotes.Size = new System.Drawing.Size(199, 279);
+            this.grdNotes.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(700, 407);
+            this.Controls.Add(this.grdNotes);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "NotesApp";
+            ((System.ComponentModel.ISupportInitialize)(this.grdNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView grdNotes;
     }
 }
 
--- NotesApp/NotesApp/Form1.cs
+++ NotesApp/NotesApp/Form1.cs
@@ -12,10 +12,38 @@ namespace NotesApp
 {
     public partial class Form1 : Form
     {
+        List<Note> _notes = new List<Note>();
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTitle.Text) && 
+                !string.IsNullOrEmpty(txtNotes.Text))
+            {
+                var note = new Note();
+                note.Title = txtTitle.Text;
+                note.Notes = txtNotes.Text;
+                _notes.Add(note);
+
+                PopulateNotes();
+                ClearForm();
+            }
+        }
+
+        private void PopulateNotes()
+        {
+            BindingSource bs = new BindingSource();
+            bs.DataSource = _notes;
+            grdNotes.DataSource = bs;
+        }
+
+        private void ClearForm()
+        {
+            txtTitle.Text = "";
+            txtNotes.Text = "";
+        }
     }
 }
